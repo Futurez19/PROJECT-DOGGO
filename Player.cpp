1234b7b05193d3b6ca0c1ff552dbb803d1a534e2
DanyFids <danyfids@gmail.com>
2019-01-26 15:16:36 -0500

    player walk
    he wolk

--- Player.cpp
+++ Player.cpp
@@ -3,7 +3,27 @@
 Player * Player::create()
 {
 	auto ret = new (std::nothrow) Player;
-	if (ret && ret->initWithFile("player.png")) {
+	if (ret && ret->initWithFile("player_anim/stand_1.png")) {
+		//still animation
+		cocos2d::Vector<cocos2d::SpriteFrame *> stillframes;
+
+		stillframes.pushBack(cocos2d::SpriteFrame::create("player_anim/stand_1.png", cocos2d::Rect(0,0,15,33) ));
+
+		//walk animation
+		cocos2d::Vector<cocos2d::SpriteFrame *> walkframes;
+		//walkframes.pushBack(stillframes.at(0));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_1.png", cocos2d::Rect(0, 0, 15, 33)));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_2.png", cocos2d::Rect(0, 0, 15, 33)));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_3.png", cocos2d::Rect(0, 0, 15, 33)));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_4.png", cocos2d::Rect(0, 0, 15, 33)));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_5.png", cocos2d::Rect(0, 0, 15, 33)));
+		walkframes.pushBack(cocos2d::SpriteFrame::create("player_anim/walk_6.png", cocos2d::Rect(0, 0, 15, 33)));
+
+		ret->animations.pushBack(cocos2d::Animation::createWithSpriteFrames(stillframes));
+		ret->animations.pushBack(cocos2d::Animation::createWithSpriteFrames(walkframes, 0.1f));
+
+		ret->runAction(cocos2d::RepeatForever::create(cocos2d::Animate::create(ret->animations.at(1))));
+
 		ret->autorelease();
 		return ret;
 	}
--- Player.h
+++ Player.h
@@ -4,9 +4,11 @@
 class Player : public cocos2d::Sprite {
 private:
 	bool face_right = true;
+	cocos2d::Vector<cocos2d::Animation *> animations;
 public:
 	static const int PLAYER_SPEED = 60;
 
 	static Player * create();
 	void move(cocos2d::Vec2 spd);
+
 };
